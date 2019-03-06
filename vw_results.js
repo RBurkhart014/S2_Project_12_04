6d2dea6ff192d2f1700158fa75992f61a545daf8
Ryan Burkhart <RBurkhart014@west-mec.org>
2019-03-05 17:44:21 -0700

    Added Comment, & Little bit of Javascript
    Clawson, Please read the comment in my project 4 javascript

--- vw_results.js
+++ vw_results.js
@@ -24,17 +24,59 @@
       
 */
 
+var reportHTML = "<h1>" + raceTitle + "</h1>";
 
+var totalVotes = 0;
 
+for (var i = 0; i < race.length; i++) {
+    votes[i].forEach(calcSum);
+    reportHTML += "<table><caption>" + race[i] + "</caption><tr><th>" + Candidate[i] + "</th><th>" + Votes[i] + "</th></tr>";
+    reportHTML += "</table>";
+}
 
+document.getElementsByTagName("section").innerHTML = reportHTML;
+
+function candidateRows(raceNum, totalVotes) {
+    var rowHTML = "";
+    for(var j = 0; j <= 2; j++) {
+        var candidateName = candidate[raceNum[j]];
+        var candidateParty = party[raceNum[j]];
+        var candidateVotes = votes[raceNum[j]];
+        var candidatePercent = calcPercent(candidateVotes, totalVotes);
+        rowHTML += "<tr><td>" + candidateName + "(" + candidateParty + ")</td><td>" + candidateVotes + "(" + candidatePercent + ")</td></tr>";
+        votes.toLocaleString();
+        candidatePercent.toFixed();
+    }
+}
 
 /* Callback Function to calculate an array sum */
 function calcSum(value) {
    totalVotes += value;
 }
 
-/* Function to calculate a percentage */
 function calcPercent(value, sum) {
    return (100*value/sum);
 }
 
+    var rowHTML = "";
+    for(var j = 0; j <= 2; j++) {
+        var candidateName = candidate[raceNum[j]];
+        var candidateParty = party[raceNum[j]];
+        var candidateVotes = votes[raceNum[j]];
+        var candidatePercent = calcPercent(candidateVotes, totalVotes);
+        rowHTML += "<tr><td>" + candidateName + "(" + candidateParty + ")</td><td>" + candidateVotes + "(" + candidatePercent + ")</td></tr>";
+        votes.toLocaleString();
+        candidatePercent.toFixed();
+    }
+
+function calcSum(value) {
+   totalVotes += value;
+}
+
+function calcPercent(value, sum) {
+   return (100*value/sum);
+}
+
+
+
+// Clawson, I had an extremely difficult time with this project. I really did not understand this material & the peers iasked for around me & worked with said the same thing, I hate to say that im giving up, but that feeling where you want to break your computer is happening and i do NOT understand this. So im turning in what i have above.
